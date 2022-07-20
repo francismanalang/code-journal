c7a82b388d980b880e9e6b269cdbd69a2b6a4161
Francis Manalang <francismanalang7@gmail.com>
2022-07-20 01:01:12 +0000

    Completed issue ready for review

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ var data = {
   nextEntryId: 1
 };
 
-var previousDataJSON = localStorage.getItem('javascript-local-storage');
+var previousDataJSON = localStorage.getItem('code-journal-local-storage');
 
 if (previousDataJSON !== null) {
   data = JSON.parse(previousDataJSON);
@@ -15,7 +15,116 @@ if (previousDataJSON !== null) {
 
 function handleStroage(event) {
   var dataJSON = JSON.stringify(data);
-  localStorage.setItem('javascript-local-storage', dataJSON);
+  localStorage.setItem('code-journal-local-storage', dataJSON);
 }
 
 window.addEventListener('beforeunload', handleStroage);
+
+var $entryForm = document.forms[0];
+var $image = document.querySelector('.image');
+
+function handleEntryForm(event) {
+  event.preventDefault();
+  var formValues = {
+    title: $entryForm.elements.title.value,
+    photoUrl: $entryForm.elements.photoUrl.value,
+    notes: $entryForm.elements.notes.value
+  };
+  getUl.prepend(journalEntries(formValues));
+  entriesContainer.className = 'container view';
+  $form.className = 'form-background hidden';
+
+  formValues.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formValues);
+  $image.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $entryForm.reset();
+  data.view = 'entries';
+}
+
+$entryForm.addEventListener('submit', handleEntryForm);
+
+function journalEntries(entry) {
+  var liElement = document.createElement('li');
+
+  var containerDiv = document.createElement('div');
+  containerDiv.setAttribute('class', 'container');
+  liElement.appendChild(containerDiv);
+
+  var rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+  containerDiv.appendChild(rowDiv);
+
+  var columnHalfImage = document.createElement('div');
+  columnHalfImage.setAttribute('class', 'column-half');
+  rowDiv.appendChild(columnHalfImage);
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photoUrl);
+  columnHalfImage.appendChild(image);
+
+  var columnHalfText = document.createElement('div');
+  columnHalfText.setAttribute('class', 'column-half');
+  rowDiv.appendChild(columnHalfText);
+
+  var h2Element = document.createElement('h2');
+  h2Element.textContent = entry.title;
+  columnHalfText.appendChild(h2Element);
+
+  var pElement = document.createElement('p');
+  pElement.textContent = entry.notes;
+  columnHalfText.appendChild(pElement);
+
+  return liElement;
+}
+
+var getUl = document.querySelector('ul');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var ulAppend = journalEntries(data.entries[i]);
+  getUl.appendChild(ulAppend);
+}
+
+window.addEventListener('DOMContentLoaded', journalEntries);
+
+var viewEntries = document.querySelector('.navbar-button');
+var entriesContainer = document.querySelector('.view');
+var $form = document.querySelector('form');
+var $noEntries = document.querySelector('.no-entries');
+
+function handleEntries(event) {
+  if (event.target === viewEntries) {
+    entriesContainer.className = 'container view';
+    $form.className = 'form-background hidden';
+  }
+  if (data.entries.length === 0) {
+    $noEntries.className = 'no-entries';
+  } else {
+    $noEntries.className = 'no-entries hidden';
+  }
+  data.view = 'entries';
+}
+
+viewEntries.addEventListener('click', handleEntries);
+
+var $newButton = document.querySelector('.new-button');
+
+function handleNewButton(event) {
+  if (event.target === $newButton) {
+    $form.className = 'form-background';
+    entriesContainer.className = 'container view hidden';
+    data.view = 'entry-form';
+  }
+}
+
+$newButton.addEventListener('click', handleNewButton);
+
+var $dataView = data.view;
+
+if ($dataView === 'entry-form') {
+  $form.className = 'form-background';
+  entriesContainer.className = 'container view hidden';
+} else {
+  $form.className = 'form-background hidden';
+  entriesContainer.className = 'container view';
+}
--- js/main.js
+++ js/main.js
@@ -8,19 +8,3 @@ function handleImage(event) {
 }
 
 $photoUrl.addEventListener('input', handleImage);
-
-function handleEntryForm(event) {
-  event.preventDefault();
-  var formValues = {
-    title: $entryForm.elements.title.value,
-    photoUrl: $entryForm.elements.photoUrl.value,
-    notes: $entryForm.elements.notes.value
-  };
-  formValues.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(formValues);
-  $image.setAttribute('src', '/images/placeholder-image-square.jpg');
-  $entryForm.reset();
-}
-
-$entryForm.addEventListener('submit', handleEntryForm);
