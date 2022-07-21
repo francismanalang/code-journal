782d91f67ad87d4cb65578cd0c73f4dadf8ead45
Francis Manalang <francismanalang7@gmail.com>
2022-07-21 01:10:39 +0000

    issue 3 edit entries progress

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var $entryForm = document.querySelector('.form-view');
 var $image = document.querySelector('.image');
 var $photoUrl = document.querySelector('.photoUrl');
+var $title = document.querySelector('.title');
+var $notes = document.querySelector('.notes');
+var $entryTitle = document.querySelector('.entry-title');
 
 function handleImage(event) {
   var photoUrlValue = $entryForm.elements.photoUrl.value;
@@ -17,9 +20,10 @@ function handleEntryForm(event) {
     photoUrl: $entryForm.elements.photoUrl.value,
     notes: $entryForm.elements.notes.value
   };
+
   getUl.prepend(journalEntries(formValues));
-  entriesContainer.className = 'container view';
-  $entryForm.className = 'form-background hidden';
+  entriesContainer.className = 'container entries-view';
+  $entryForm.className = 'form-view hidden';
 
   formValues.entryId = data.nextEntryId;
   data.nextEntryId++;
@@ -27,12 +31,17 @@ function handleEntryForm(event) {
   $image.setAttribute('src', '/images/placeholder-image-square.jpg');
   $entryForm.reset();
   data.view = 'entries';
+
+  // if (formValues === data.editing) {
+  //   formValues.replaceWith(data.editing);
+  // }
 }
 
 $entryForm.addEventListener('submit', handleEntryForm);
 
 function journalEntries(entry) {
   var liElement = document.createElement('li');
+  liElement.setAttribute('data-entry-id', entry.entryId);
 
   var containerDiv = document.createElement('div');
   containerDiv.setAttribute('class', 'container');
@@ -56,7 +65,12 @@ function journalEntries(entry) {
 
   var h2Element = document.createElement('h2');
   h2Element.textContent = entry.title;
+  h2Element.setAttribute('class', 'display-flex');
   columnHalfText.appendChild(h2Element);
+
+  var penIcon = document.createElement('i');
+  penIcon.setAttribute('class', 'fa-solid fa-pen pen');
+  h2Element.appendChild(penIcon);
 
   var pElement = document.createElement('p');
   pElement.textContent = entry.notes;
@@ -64,6 +78,30 @@ function journalEntries(entry) {
 
   return liElement;
 }
+
+var $ulElement = document.querySelector('.entries-list');
+
+function handleUl(event) {
+  if (event.target.tagName === 'I') {
+    var closestLiAttributeString = event.target.closest('li').getAttribute('data-entry-id');
+    var closestLiAttribute = Number(closestLiAttributeString);
+    $entryForm.className = 'form-view';
+    entriesContainer.className = 'container entries-view hidden';
+    data.view = 'entry-form';
+  }
+  for (var i = 0; i < data.entries.length; i++) {
+    if (closestLiAttribute === data.entries[i].entryId) {
+      data.editing = data.entries[i];
+      $title.value = data.editing.title;
+      $photoUrl.value = data.editing.photoUrl;
+      $notes.value = data.editing.notes;
+      $image.setAttribute('src', data.editing.photoUrl);
+      $entryTitle.textContent = 'Edit Entry';
+    }
+  }
+}
+
+$ulElement.addEventListener('click', handleUl);
 
 function handleContentLoaded(event) {
   var getUl = document.querySelector('ul');
@@ -75,11 +113,11 @@ function handleContentLoaded(event) {
 
   var $dataView = data.view;
   if ($dataView === 'entry-form') {
-    $entryForm.className = 'form-background';
-    entriesContainer.className = 'container view hidden';
+    $entryForm.className = 'form-view';
+    entriesContainer.className = 'container entries-view hidden';
   } else if ($dataView === 'entries') {
-    $entryForm.className = 'form-background hidden';
-    entriesContainer.className = 'container view';
+    $entryForm.className = 'form-view hidden';
+    entriesContainer.className = 'container entries-view';
   }
 }
 
@@ -91,8 +129,8 @@ var $noEntries = document.querySelector('.no-entries');
 
 function handleEntries(event) {
   if (event.target === viewEntries) {
-    entriesContainer.className = 'container view';
-    $entryForm.className = 'form-background hidden';
+    entriesContainer.className = 'container entries-view';
+    $entryForm.className = 'form-view hidden';
   }
   if (data.entries.length === 0) {
     $noEntries.className = 'no-entries';
@@ -108,8 +146,11 @@ var $newButton = document.querySelector('.new-button');
 
 function handleNewButton(event) {
   if (event.target === $newButton) {
-    $entryForm.className = 'form-background';
-    entriesContainer.className = 'container view hidden';
+    $entryForm.className = 'form-view';
+    entriesContainer.className = 'container entries-view hidden';
+    $entryForm.reset();
+    $image.setAttribute('src', '/images/placeholder-image-square.jpg');
+    data.editing = null;
     data.view = 'entry-form';
   }
 }
